bd9806adccc6c588a85a9a5adaf395a3c60504f7
AsianMustache <phany280687@mail.ru>
2024-01-11 14:03:29 +0300

    fix: исправляет компонент Card

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -19,7 +19,7 @@ function Card({ card, onCardClick, onCardLike, onDeletePopupClick }) {
 
   const currentUser = useContext(CurrentUserContext);
   const isOwn = card.owner === currentUser._id;
-  const isLiked = card.likes.some((i) => i._id === currentUser._id);
+  const isLiked = card.likes.some((i) => i === currentUser._id);
   const cardLikeButtonClassName = `element__group-favorite ${
     isLiked ? "element__group-favorite_active" : ""
   }`;
