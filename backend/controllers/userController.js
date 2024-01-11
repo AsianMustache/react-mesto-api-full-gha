22f169ed4f4720da4c66e40772743f7a967be0e3
AsianMustache <phany280687@mail.ru>
2024-01-11 21:24:03 +0300

    fix: добавляет консоль для вывода currentUser в Card

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -142,6 +142,7 @@ exports.getCurrentUser = async (req, res, next) => {
       email: user.email,
       about: user.about,
       avatar: user.avatar,
+      _id: user._id,
     });
   } catch (error) {
     next(error);
